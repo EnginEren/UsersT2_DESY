6d5e7906e292725b0cc35314a5587e637219ca9c
Engin Eren <eren@nafhh-cms05.desy.de>
2015-07-24 11:00:17 +0200

    Adding DN condition

--- generate_html.sh
+++ generate_html.sh
@@ -6,7 +6,7 @@ set -f
 comm -12 $1 $2 | awk '{print $1,$2}' > notouch
 
 cat notouch | awk '{print $1}' | sort -u > onlyname
-cat /nfs/dust/cms/user/eren/SiteDB.output | awk '{print $1}' | sort -u > cms_users
+cat /nfs/dust/cms/user/eren/output_T2Users/SiteDB.output | awk '{print $1}' | sort -u > cms_users
 
 
 touch users.html
@@ -25,22 +25,23 @@ Users@T2_DESY
 user_flag=false
 for i in $(cat onlyname); do
 	touch user_$i.html
-	user_size=$(cat lists/user.files_billing_size.txt| grep $i | awk '{print $3}' | awk '{s+=$1} END {print s}' | awk '{print $1/1024^3}')
+	user_size=$(cat /nfs/dust/cms/user/eren/output_T2Users/user.files_billing_size.txt| grep $i | awk '{print $3}' | awk '{s+=$1} END {print s}' | awk '{print $1/1024^3}')
 	echo "<!DOCTYPE html>
 <html>
 <body>
 <BODY BACKGROUND="speckle.gif">
 <h2>Dear $i, you did not access these files in tier2/store/user directory over 12 months. Size : $user_size GB </h2> " > user_$i.html
-	cat cms_users | grep $i > /dev/null
+	cat cms_users | grep '\<'$i'\>' > /dev/null
 	if [ "$?" == "1" ]; then
 		user="$i"
 		echo "<ul>
-		<li> <p><a href="https://eeren.web.cern.ch/eeren/user_$i.html"> $i (non-CMS)</a></p> </li> 
+		<li> <p><a href="https://eeren.web.cern.ch/eeren/user_$i.html"> $i (<font color="red">No DN mapping is found</font>)</a></p> </li> 
 	        </ul>" >> users.html
 	else
 		user="$i"
+		dn=$(cat /nfs/dust/cms/user/eren/output_T2Users/SiteDB.output | grep '\<'$i'\>' | awk '{print $2}') 
                 echo "<ul>
-                <li> <p><a href="https://eeren.web.cern.ch/eeren/user_$i.html"> $i </a></p> </li>
+                <li> <p><a href="https://eeren.web.cern.ch/eeren/user_$i.html"> $i ($dn)</a></p> </li>
                 </ul>" >> users.html
 	fi
         echo "<pre>" >> user_$i.html
