d856998d272c065156675a9405aaa45449997998
Engin Eren <eren@nafhh-cms05.desy.de>
2015-07-29 11:42:28 +0200

    bug fixed

--- generate_html.sh
+++ generate_html.sh
@@ -2,10 +2,12 @@
 
 IFS=$'\n'
 set -f
+PBS=/nfs/dust/cms/user/eren
 
-comm -12 $1 $2 | awk '{print $1,$2}' > notouch
+#comm -12 $1 $2 | awk '{print $1,$2}' > notouch
+cat $PBS/output_T2Users/user.files_billing.txt | awk '{print $1,$2}' > notouch
 
-cat notouch | awk '{print $1}' | sort -u > onlyname
+cat $PBS/output_T2Users/user.files_billing.txt | awk '{print $1}' | sort -u > onlyname
 cat /nfs/dust/cms/user/eren/output_T2Users/SiteDB.output | awk '{print $1}' | sort -u > cms_users
 
 
@@ -20,7 +22,8 @@ Users@T2_DESY
 </title>
 </head>
 <body>
-<h2> Users who did not access/touch files in tier2/store/user directory over 12 months </h2>
+<h2> Users who did not access/touch files in tier2/store/user directory over 12 months. </h2>
+<h2> People who do not have DN mapping with CMS Site-Database have total amount of : </h2>
 <img src="usergroup.png" alt="People" style="width:256px" align="right"> " > users.html
 user_flag=false
 for i in $(cat onlyname); do
@@ -34,6 +37,7 @@ for i in $(cat onlyname); do
 	cat cms_users | grep '\<'$i'\>' > /dev/null
 	if [ "$?" == "1" ]; then
 		user="$i"
+		echo $user_size >> noncms
 		echo "<ul>
 		<li> <p><a href="https://eeren.web.cern.ch/eeren/user_$i.html"> $i (<font color="red">No DN mapping is found</font>)</a></p> </li> 
 	        </ul>" >> users.html
@@ -60,5 +64,10 @@ rm notouch onlyname cms_users
 echo "Now putting everything into lxplus..."
 #scp *.html eeren@lxplus.cern.ch:~/www
 #mv *.html
+NONcms=$(cat noncms | awk '{print $1}' | awk '{s+=$1} END {print (s/1024)}')
+Line_num=$(cat users.html | grep -n amount | awk '{print $1}' | cut -d : -f1)  
+sed -i ''$Line_num's/.*/<h2> People who do not have DN mapping with CMS Site-Database have total '$NONcms' TB<\/h2>/' users.html
+
+rm noncms
 
 exit 0;
